19faf935ed4e5d62eebcb7ce4ec798cbfae766d1
James Cook <jamescook.106@gmail.com>
2016-05-03 14:36:44 +0100

    Added the activeadmin

--- do_one_thing/config/routes.rb
+++ do_one_thing/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get "/:page" => "welcome#show"
 
   # The priority is based upon order of creation: first created -> highest priority.
