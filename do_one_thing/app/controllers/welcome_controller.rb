ac1d65e504515735e6ae5dcdc85ee03b02980c03
James Cook <jamescook.106@gmail.com>
2016-05-03 12:30:54 +0100

    Added a home page, and updated the Nav Bar

--- do_one_thing/app/controllers/welcome_controller.rb
+++ do_one_thing/app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+
+   def show
+      if valid_page?
+        render template: "welcome/#{params[:page]}"
+      else
+        render file: "public/404.html", status: :not_found
+      end
+    end
+
+    private
+    def valid_page?
+      File.exist?(Pathname.new(Rails.root + "app/views/welcome/#{params[:page]}.html.erb"))
+    end
 end
