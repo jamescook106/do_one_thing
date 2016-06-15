c11263faa5ddc9ccf1cbee8699723e5cadf24e09
James Cook <jamescook.106@gmail.com>
2016-06-15 15:40:09 +0100

    Most clicked thumbnail resizing and new header bar

--- do_one_thing/app/models/job.rb
+++ do_one_thing/app/models/job.rb
@@ -1,6 +1,9 @@
 class Job < ActiveRecord::Base
 	include Impressionist::IsImpressionable
-	has_attached_file :photo
+	has_attached_file :photo,
+	:styles =>{
+		:thumb =>"400x300#"
+	}
 	do_not_validate_attachment_file_type :photo
 	is_impressionable :counter_cache => true
 end
