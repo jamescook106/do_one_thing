5790168346506209c3e0a827c02247f45a0c75d4
James Cook <jamescook.106@gmail.com>
2016-06-15 16:59:41 +0100

    Advertised role alignment

--- do_one_thing/app/models/job.rb
+++ do_one_thing/app/models/job.rb
@@ -2,7 +2,9 @@ class Job < ActiveRecord::Base
 	include Impressionist::IsImpressionable
 	has_attached_file :photo,
 	:styles =>{
-		:thumb =>"400x300#"
+		:thumb =>"400x300#",
+		:primary => "900x450#",
+		:secondary => "400x200#"
 	}
 	do_not_validate_attachment_file_type :photo
 	is_impressionable :counter_cache => true
