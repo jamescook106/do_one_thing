8bbe1c5ee0bd4b29142644c49ea5732d34c06736
James Cook <jamescook.106@gmail.com>
2016-06-28 18:52:01 +0100

    s3

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,12 +1,12 @@
 class Job < ActiveRecord::Base
 	include Impressionist::IsImpressionable
 	has_attached_file :photo,
+	storage: s3,
 	:styles =>{
 		:thumb =>"400x300#",
 		:primary => "900x450#",
 		:secondary => "400x200#"
 	}
-	storage: :s3,
 	do_not_validate_attachment_file_type :photo
 	is_impressionable :counter_cache => true
 end
