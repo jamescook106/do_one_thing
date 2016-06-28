2ccb5f29462771531417e1672a83a8e2e06f4589
James Cook <jamescook.106@gmail.com>
2016-06-28 21:30:44 +0100

    typo

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,5 +5,5 @@ Paperclip::Attachment.default_options.merge!({
 		access_key_id: ENV['AWS_ACCESS_KEY_ID'],
 		secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
 	},
-	bucket: ENV['S3_BUCKET_NAME']
+	s3_bucket: ENV['S3_BUCKET_NAME']
 })
