fbcc2d3c2000ba9ec3328f77ab8644e9fb0d21b8
James Cook <jamescook.106@gmail.com>
2016-06-28 21:43:42 +0100

    Reset to non-s3

--- config/initializers/paperclip.rb
+++
@@ -1,9 +0,0 @@
-Paperclip::Attachment.default_options.merge!({
-	storage: :s3,
-	s3_host_name: 's3-us-west-2.amazonaws.com',
-	s3_credentials: {
-		access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-		secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-	},
-	s3_bucket: ENV['S3_BUCKET_NAME']
-})
