ec2a3d0fb05fa14032b3b67da7d76c3398d111b0
James Cook <jamescook.106@gmail.com>
2016-06-30 13:46:08 +0100

    do1thing logo link

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,9 +1,4 @@
 RailsAdmin.config do |config|
-  #config.authorize_with do |controller|
-  #  unless current_user.try(:admin?)
-  #    redirect_to main_app.root_url
-  #  end
-  #end
 
   ### Popular gems integration
 
