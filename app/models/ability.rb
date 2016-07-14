8c40bf42a0c05350df1874c7e268187c08610b80
James Cook <jamescook.106@gmail.com>
2016-07-14 11:23:03 +0100

    Ruby models and controllers comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,36 +1,24 @@
+#Ability.rb
+#This file contains the settings for cancan, and hence manages what level of
+#admin can access what parts of the site
+
 class Ability
+
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
+    # Define abilities for the passed in user here.
+    #If Sadmin or Admin we can access the admin system
+    #Only the Sadmin can manage other admin
        if user.sadmin? or user.admin?
          can :access, :rails_admin
-         can :dashboard 
+         can :dashboard
          can :manage, [Job]
          if user.sadmin?
             can :manage, :all
         end
        else
        end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+
   end
 end
