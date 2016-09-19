1aec6a25e751ee81c1577fc6010a3c249838d20c
James Cook <jamescook.106@gmail.com>
2016-09-19 18:50:17 +0100

    Copyright added on the Admin system

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -35,7 +35,7 @@ RailsAdmin.config do |config|
     label 'Opportunity'
     edit do
       field :name do
-        required true 
+        required true
         label 'Headline'
         help 'This is the ‘headline’ for your opportunity.  Keep it short and snappy'
       end
@@ -47,22 +47,22 @@ RailsAdmin.config do |config|
         help 'These buttons determine which opportunities feature in the ‘Featured Opportunities’ section.  Please let the Sadmins know if you would like to ‘feature’ your opportunity.  Leave unchecked.'
       end
       field :url do
-        required true 
+        required true
         label 'Internal URL Linking'
         help 'This is the internal linking code used by the website.  Choose one word from your headline.'
       end
       field :tagline do
-        required true 
+        required true
         label 'Hover Over Image Text'
         help 'This is the ‘hover over’ text used on the images. It could be the same as the Headline but it would be good to include a bit more'
       end
       field :short do
-        required true 
+        required true
         label 'Short Text'
         help 'This is the text on the front page of the website.  Try to make it attractive and obviously related to KCL researchers'
       end
       field :content do
-        required true 
+        required true
         label 'Long Text'
         help 'This is the long text you see after you click through.  Make clear if the event is JUST for KCL researchers.  Include instructions for the Further Information section if necessary.'
       end
@@ -82,9 +82,12 @@ RailsAdmin.config do |config|
         help 'This is the date you want the opportunity to leave the website.  It is fine to keep something on there for a couple of days after the closing date, to demonstrate the range of possible'
       end
       field :photo do
-        required true 
+        required true
         label 'Image'
         help 'Please insert an image.  Make absolutely sure the image is copyright-free or used with permission.  Landscape images are better; make sure any text is in the centre of the image as the thumbnails cut into them.'
+      end
+      field :copyright do
+        help 'If your image is copyrighted please supply the relevant source'
       end
     end
   end
