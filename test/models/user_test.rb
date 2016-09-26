1414832173aa62af9a89dcd55f7398647df58013
James Cook <jamescook.106@gmail.com>
2016-09-26 19:20:17 +0100

    Added a test database and added some tests to show that users are created as non-admin non-sadmin

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  test "Default User is not admin" do
+    user=User.new
+    user.save
+    if user.admin
+      assert false, 'Default User is an admin'
+    else
+      assert true, 'Default User is not an admin'
+    end
+  end
+
+  test "Default User is not sadmin" do
+    user=User.new
+    user.save
+    if user.sadmin
+      assert false, 'Default User is an sadmin'
+    else
+      assert true, 'Default User is not an sadmin'
+    end
+  end
+
   # test "the truth" do
   #   assert true
   # end
