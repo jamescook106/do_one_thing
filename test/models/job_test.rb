c47967a61f60ea79f028acfb6a5c9c2cb0a52672
James Cook <jamescook.106@gmail.com>
2016-09-26 20:41:57 +0100

    Added a few more tests for job and user models

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "New Job has impressionist count 0" do
+    job=Job.new
+    job.save
+    if job.impressions_count==0
+      assert true, 'impressions_count default working'
+    else
+      assert false, 'impressions_count default not working'
+    end
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -21,4 +21,27 @@ class UserTest < ActiveSupport::TestCase
       assert true, 'Default User is not an sadmin'
     end
   end
+
+  test "Sadmin creation confirmation" do
+    user=User.new
+    user.sadmin=true
+    user.save
+    if user.sadmin
+      assert true, 'Sadmin creation successful'
+    else
+      assert false, 'Sadmin creation unsuccessful'
+    end
+  end
+
+  test "admin creation confirmation" do
+    user=User.new
+    user.admin=true
+    user.save
+    if user.admin
+      assert true, 'admin creation successful'
+    else
+      assert false, 'admin creation unsuccessful'
+    end
+  end
+
 end
