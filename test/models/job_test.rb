b1a606993a33ab20469599ec8e7d214091801653
James Cook <jamescook.106@gmail.com>
2016-12-07 11:20:47 +0000

    Creation of a User test

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
+
+  test "Create a new Job" do
+    job=Job.new
+    job.save
+    assert true, 'Opportunity Create'
+  end
+
   test "New Job has impressionist count 0" do
     job=Job.new
     job.save
