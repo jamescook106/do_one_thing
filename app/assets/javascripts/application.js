952dcecd7e97610e1c72e71b73f0ab8befe5c5a0
James Cook <jamescook.106@gmail.com>
2017-07-26 13:54:15 +0100

    Load Masonary after images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,12 +25,11 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 // init Masonry
-var $grid = $('.grid').masonry({
-  // options...
-});
-// layout Masonry after each image loads
-$grid.imagesLoaded().progress( function() {
-  $grid.masonry('layout');
+var $grid = $('.grid').imagesLoaded( function() {
+  // init Masonry after all images have loaded
+  $grid.masonry({
+    // options...
+  });
 });
 
 $(function(){ $(document).foundation(); });
